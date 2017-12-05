203b68b5401b73a4b167617a96ff245b42b7d109
Hamdi Sparda <Hamdi.Sparda@hotmail.com>
2017-12-05 02:29:40 +0100

    added login to apps/fixed url routing/added base.html(home page) and its static files css/js/fonts

--- login/views.py
+++ login/views.py
@@ -2,5 +2,8 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+#from django.http import Http.Response
 
 # Create your views here.
+def home(request):
+    return render(request, "base.html", {})
--- studentsunited/urls.py
+++ studentsunited/urls.py
@@ -17,12 +17,14 @@ from django.conf import settings
 from django.conf.urls import url
 from django.conf.urls.static import static
 from django.contrib import admin
+#from django.contrib.auth import views as auth_views
+from login import views as login_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    #url(r'^admin/', admin.site.urls),
+    url(r'^home/$', login_views.home, name='home'),
 ]
-
+#'login.views.home' replaced with home,
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
