cbd7221435cfe8da96fce15b57d3a4f683625492
Hamdi Sparda <Hamdi.Sparda@hotmail.com>
2017-12-07 23:31:51 +0100

    SMTP fixed & confirmed to be working

--- login/views.py
+++ login/views.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.core.mail import send_mail
 from django.shortcuts import render
 #from django.http import Http.Response
 
--- studentsunited/settings.py
+++ studentsunited/settings.py
@@ -28,7 +28,7 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'studentunitedproject@gmail.com'
+EMAIL_HOST_USER = 'studentsunitedproject@gmail.com'
 EMAIL_HOST_PASSWORD = 'sup777999'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
