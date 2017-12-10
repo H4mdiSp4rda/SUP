49ab3cd7d3f69c034b75aeacd66de56c9c2e01c9
Hamdi Sparda <Hamdi.Sparda@hotmail.com>
2017-12-10 02:40:29 +0100

    changed-to-portfolio-template

--- studentsunited/settings.py
+++ studentsunited/settings.py
@@ -29,7 +29,7 @@ ALLOWED_HOSTS = []
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'studentsunitedproject@gmail.com'
-EMAIL_HOST_PASSWORD = 'sup777999'
+EMAIL_HOST_PASSWORD = '!QEu&6CVSEu8xbfB'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
